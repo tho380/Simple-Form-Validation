ee391ea8271b1a3b1e6e1e41d681247aaa358d8f
tho380 <56561331+tho380@users.noreply.github.com>
2020-09-02 13:52:00 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const form = document.querySelector('form');
+const fname = document.getElementById('first-name');
+const lname = document.getElementById('last-name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+// Show input error message
+function showError(input, message) {
+  const formGroup = input.parentElement;
+  formGroup.className = 'form-group error';
+  const small = formGroup.querySelector('small');
+  small.innerText = message;
+}
+
+// Show input success 
+function showSuccess(input, message) {
+  const formGroup = input.parentElement;
+  formGroup.className = 'form-group';
+  const small = formGroup.querySelector('small');
+  small.innerText = message;
+}
+
+// Check email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input, '');
+    }
+  });
+}
+
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+  } else {
+    showSuccess(input, '');
+  }
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  checkRequired([fname, lname, email, password]);
+  checkLength(password, 5, 25);
+})
